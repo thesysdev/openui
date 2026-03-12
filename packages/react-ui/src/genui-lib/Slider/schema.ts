3993241bf33b0151e6dd3ad775ce88ccf00b6369
i-subham <i-subham@thesys.dev>
2026-03-12 18:27:59 +0530

    Update global CSS to import styles from the new @openuidev/react-ui package and modify Slider schema to make the label property optional.

--- packages/react-ui/src/genui-lib/Slider/schema.ts
+++ packages/react-ui/src/genui-lib/Slider/schema.ts
@@ -3,11 +3,11 @@ import { rulesSchema } from "../rules";
 
 export const SliderSchema = z.object({
   name: z.string(),
-  label: z.string().optional(),
   variant: z.enum(["continuous", "discrete"]),
   min: z.number(),
   max: z.number(),
   step: z.number().optional(),
   defaultValue: z.array(z.number()).optional(),
+  label: z.string().optional(),
   rules: rulesSchema,
 });
