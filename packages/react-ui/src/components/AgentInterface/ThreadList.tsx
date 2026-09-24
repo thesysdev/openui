1a94209b7a352200268d0dfadf2d87794bb92073
Abhin Rustagi <abhin@thesys.dev>
2026-09-24 23:20:23 +0530

    fix(react-ui): chat selector condition

--- packages/react-ui/src/components/AgentInterface/ThreadList.tsx
+++ packages/react-ui/src/components/AgentInterface/ThreadList.tsx
@@ -59,7 +59,8 @@ export const ThreadButton = ({
       className={clsx(
         "openui-agent-thread-button",
         {
-          "openui-agent-thread-button--selected": selectedThreadId === id,
+          "openui-agent-thread-button--selected":
+            selectedThreadId === id && nav?.path === undefined,
           "openui-agent-thread-button--actions-open": isActionsOpen,
         },
         className,
